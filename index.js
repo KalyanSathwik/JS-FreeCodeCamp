e8daf8c4fbf5702b5399ffd71fe86a3fa3a28ca1
MADHIRE KALYAN SATHWIK <madhirekalyansathwik@gmail.com>
2025-08-03 09:45:16 +0530

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 let count = 0;
-count = document.getElementById('count-el');
-val = count
+let countel = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+console.log(count)
+let welcomeEl = document.getElementById('welcome-el')
+let name = "Kalyan"
+let greeting = 'Hello ' + name + ' Good Morning, Have a nice day'
+welcomeEl.textContent = greeting
+
 function increment(){
-    val++;
-    count = val;
+    count++;
+    countel.textContent = count;
+    console.log(count)
+    console.log("Clicked")
+}
+
+function save(){
+    let countStr = count;
+    saveEl.textContent += countStr + " - "
+    countel.textContent = 0
+    count = 0;
+}
+
+let errorEl = document.getElementById('errorpara')
+let errormsg = "Error, Please try again later!"
+function purchase(){
+    errorEl.textContent = errormsg
 }
